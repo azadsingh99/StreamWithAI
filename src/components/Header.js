5bcffc9362f4e45cacc9e9b5c543d12842a58cb1
azadsingh99 <azadsingh1934278@gmail.com>
2024-09-29 12:25:37 +0530

    Integrated the SignUp & SignIn with the Google

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,13 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10" > 
-        <img className="w-44" src='
-https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'  alt='Logo' />
+        <img className="w-44 bg-black" src='https://i.postimg.cc/Gm7hkdtm/STERAM-AI-9-29-2024.gif'  alt='Logo' />
     </div>
   )
 }
 
+
 export default Header
+
+
+ 
